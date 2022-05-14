db4bbe7861216e8bfdd7dc5c43dc25031dd53286
navaneeth20000 <74000541+navaneeth20000@users.noreply.github.com>
2022-05-14 23:19:21 +0530

    Add files via upload

---
+++ src/pages/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from "../components/CheckoutForm";
+
+const Checkout = () => {
+    const stripePromise = loadStripe('pk_test_9UyT4Sg5mj6ZX052Nffhxvqo00BkvywDH5');
+
+    return (
+        <section className="checkout-wrapper">
+            <AmplifyAuthenticator>
+                <Elements stripe={stripePromise}>
+                    <section>
+                        <h2>Time to Checkout?</h2>
+                        <CheckoutForm />
+                    </section>
+                </Elements>
+            </AmplifyAuthenticator>
+        </section>
+    )
+}
+
+export default Checkout
